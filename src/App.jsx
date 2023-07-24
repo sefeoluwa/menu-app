d832867ed635e2a9c866428aa67ee84ada90b9c7
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-24 14:10:20 +0100

    removed unused files and created base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import {About, Footer, Hero, Navbar} from "./components"
 
 function App() {
   
   return (
     <>
-      
+      <Navbar />
+      <Hero />
+      <About />
+      <Footer />
+
     </>
   )
 }

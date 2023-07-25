ff3e05faa69140d0112f43e357f15c3910fbfae6
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-25 19:36:28 +0100

    added all project assets

---
+++ src/assets/index.js
@@ -0,0 +1,30 @@
+import appstore from "appstore.png"
+import close from "close.svg"
+import menu from "menu.svg"
+import Facebook from "Facebook.png"
+import googleplay from "googleplay.png"
+import home6 from "home6.png"
+import home7 from "home7.png"
+import home8 from "home8.png"
+import home9 from "home9.png"
+import home31 from "home31.png"
+import Instagram from "Instagram.png"
+import logo from "logo.png"
+import Twitter from "Twitter.png"
+
+
+export {
+    appstore,
+    close,
+    menu,
+    Facebook,
+    googleplay,
+    home6,
+    home7,
+    home8,
+    home9,
+    home31,
+    Instagram,
+    logo,
+    Twitter
+}

3d709d3cb1c37a8708bef4dd3eee1d8a2f83d59a
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-25 23:39:55 +0100

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import {About, Footer, Hero, Navbar} from "./components"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
   
   return (
-    <>
+    <BrowserRouter>
+      <div className="relative ">
       <Navbar />
       <Hero />
       <About />
+      </div>
       <Footer />
 
-    </>
+    </BrowserRouter>
   )
 }
 

6fac42481f1e02b1607cb761af0450c2c5272489
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-27 00:25:54 +0100

    alignment for hero section images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,29 @@
 import { motion } from 'framer-motion';
-import { heroScreens } from "../constants"
-import { styles } from "../styles"
+import { heroScreens } from "../constants";
+import { styles } from "../styles";
 
 
 function Hero() {
+  const marginTopForSecondImage = 'mt-3';
   return (
     <>
-      <p className={`${styles.heroHeadText} pt-16`}>Be a Part of My Menu</p>
-      <div className="hero-div">
-      <div className='flex flex-row mt-9'>{heroScreens.map((homeScreens) => (
-        <div className="" key={homeScreens.name}>
-        <img
-              src={homeScreens.image}
-              alt={homeScreens.name}
-              className=""
-            />
-        </div>
-      ))}
+          <p className={`${styles.heroHeadText} pt-16`}>Be a Part of My Menu</p>
+      <div className=" mt-9 div-map">
+        {heroScreens.map((homeScreens, index) => {
+          
+         return (
+            <div className="p-2" key={homeScreens.name}>
+              <img
+                src={homeScreens.image}
+                alt={homeScreens.name}
+                className={` ${index === 1 ? marginTopForSecondImage : ''}`}
+              />
+            </div>
+          );
+        })}
       </div>
-      </div>
-      
     </>
-    
-  )
+  );
 }
 
-export default Hero
+export default Hero;

480c7321e177b070c2a488f0ca2d5e7d3414b748
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-26 22:02:18 +0100

    design an alignment for larger screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,50 @@
-import React, { useState} from "react";
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
-
-
-
-
+import { logo } from "../assets";
 
 const Navbar = () => {
-
   const [active, setActive] = useState("");
-const [toggle, setToggle] = useState(false);
-const [scrolled, setScrolled] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   return (
-    <nav  className={`${ styles.paddingX } w-full flex items-center py-4 fixed top-0 z-50`}>
-    <div className="w-full flex justify-start items-center max-w-7xl mx-auto">
-        <Link to="/" 
-        className="flex items-center gap-2"  
-        onClick={() => {setActive("");
-        window.scrollTo(0,0)}}>
+    <nav
+      className={`${
+        styles.paddingX
+      } w-full flex items-center justify-between py-4 fixed top-0 z-50 bg-primaryColor`}
+    >
+      <div className="flex items-center gap-2">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-[30px] font-bold cursor-pointer">My Menu</p>
+        </Link>
+      </div>
 
-          <div className="flex ">
-           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-            <p className=' text-[20px] font-bold cursor-pointer flex'>My Menu</p>
-          </div>
-       </Link>
 
-       <ul className="list-none hidden sm:flex flex-row  ml-24 gap-10">
-        {navLinks.map((Link) => ( 
-          <li
-          key={Link.id}
-          className={`${
-            active === Link.title ? "text-white" : "text-secondary"
-          }hover:text-white text-[18px] font-medium cursor-pointer`}
-          onClick={() => setActive(Link.title)}
-          >
-            <a href={`#${Link.id}`}>{Link.title}</a>
-          </li>
-        ))}
+      <div className="flex justify-center -ml-44 flex-1">
+        <ul className="list-none hidden justify-between w-1/4  sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className="text-[20px] pl-10 font-medium cursor-pointer"
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
         </ul>
-     </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

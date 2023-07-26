62e587f70b03fb722a2fc10d575ff686506b171b
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-26 20:20:36 +0100

    declared base styles

---
+++ src/styles.js
@@ -0,0 +1,13 @@
+const styles = {
+    primaryColor: "#E7DF2F",
+    secondaryColor: "#fff",
+
+    paddingX: "sm:px-16 px-6",
+    paddingY: "sm:py-16 py-6",
+    padding: "sm:px-16 px-6 sm:py-16 py-10",
+
+    heroHeadText: 
+    "text-center font-poppins text-6xl  font-semibold leading-9"
+}
+
+export { styles }

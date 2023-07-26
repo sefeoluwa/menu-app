501af5bbf715b610430bd15006d9f1dfd7ab569d
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-26 20:22:06 +0100

    imported dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState} from "react";
+import { Link } from 'react-router-dom';
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+
+    </nav>
   )
 }
 

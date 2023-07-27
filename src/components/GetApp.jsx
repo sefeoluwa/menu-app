c5a03ed0fb3face381fc381eeec1ef747765c266
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-27 19:43:50 +0100

    padding to download links

--- src/components/GetApp.jsx
+++ src/components/GetApp.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+import { motion } from "framer-motion";
+import SectionWrapper from '../hoc/SectionWrapper';
+import { download } from "../constants";
+
 
 function GetApp() {
   return (
-    <div>GetApp</div>
+    <>
+      <motion.p className="flex justify-center text-[20px]">Get the App and begin to search the menu of your favourte restaurants.</motion.p>
+      <motion.div className="flex justify-center items-center  p-5">
+        {download.map((download) => { 
+          return (
+          <img 
+          key={download.name} 
+          src={download.image}
+          className="p-6 cursor-pointer"
+           />
+        )
+        })}
+      </motion.div>
+    </>
   )
 }
 
-export default GetApp
+export default SectionWrapper(GetApp, '')

a42faeca7265e5445aa8f6659fbf26dbc45dfbf2
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-27 18:55:14 +0100

    rectangular box added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,9 @@ function Hero() {
     <>
           <p className={`${styles.heroHeadText} pt-16`}>Be a Part of My Menu</p>
       <div className=" mt-9 div-map">
-        {heroScreens.map((homeScreens, index) => {
-          
+        {heroScreens.map((homeScreens, index) => { 
          return (
-            <div className="p-2" key={homeScreens.name}>
+            <div className="p-2 z-10" key={homeScreens.name}>
               <img
                 src={homeScreens.image}
                 alt={homeScreens.name}
@@ -21,7 +20,9 @@ function Hero() {
             </div>
           );
         })}
+        <div className="rectangular-box absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
       </div>
+      
     </>
   );
 }

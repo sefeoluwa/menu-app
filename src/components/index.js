bfe2027885e799a6aa12f2219937fda3b39f0d5e
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-27 18:56:48 +0100

    added new component

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import About from "./About";
 import Footer from "./Footer"
+import GetApp from "./GetApp"
 
 export {
     Navbar,
     Hero,
     About,
     Footer,
+    GetApp,
 }

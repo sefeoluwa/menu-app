1526d285a0211aa8d12a6dda1566efa074f54ef7
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-28 18:37:24 +0100

    display flex for li links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
 import { company, help, icons, terms } from '../constants'
 import { motion } from 'framer-motion';
-import SectionWrapper from '../hoc/SectionWrapper';
 import { textVariant } from '../utils/motion';
 
 function Footer() {
   return (
    <>
-    <motion.div variants={textVariant()} className='pt-[5vh]'>
+    <motion.div variants={textVariant()} className='pt-[5vh] flex justify-around mr-7 footer'>
     <div>
-      <h3>My Menu</h3>
+      <h3 className="text-[35px] font-bold">My Menu</h3>
       <p>Search the menu of your favorite restaurants</p>
-      <div className="">{icons.map((icons) => (
-        <img src={icons.icon} key={icons.name} className='m-3 bg-black'/>
+      <div className="flex ">{icons.map((icons) => (
+        <img src={icons.icon} key={icons.name} className='m-3  cursor-pointer img'/>
       ))}</div>
     </div>
 
-    <div className="">
-    <ul className="list-none hidden z-50 justify-between w-1/4  sm:flex flex-row gap-10">
+    <div className="company">
+    <p>Company</p>
+    <ul className="list-none z-50 justify-between">
           {company.map((link) => (
             <li
               key={link.id}
-              className="text-[20px] pl-10 font-medium cursor-pointer"
+              className="text-[20px] pl-10 font-medium cursor-pointer p-3"
               >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
@@ -29,12 +29,13 @@ function Footer() {
         </ul>
     </div>
 
-    <div className="">
-    <ul className="list-none hidden z-50 justify-between w-1/4  sm:flex flex-row gap-10">
+    <div className="help">
+    <p>Help</p>
+    <ul className="list-none z-50  flex flex-col justify-between">
           {help.map((link) => (
             <li
               key={link.id}
-              className="text-[20px] pl-10 font-medium cursor-pointer"
+              className="text-[20px] pl-10 font-medium cursor-pointer p-3"
               
             >
               <a href={`#${link.id}`}>{link.title}</a>
@@ -43,12 +44,13 @@ function Footer() {
         </ul>
     </div>
 
-    <div className="">
-    <ul className="list-none hidden z-50 justify-between w-1/4  sm:flex flex-row gap-10">
+    <div className="terms">
+    <p>Terms</p>
+    <ul className="list-none z-50 justify-between flex flex-col w-auto">
           {terms.map((link) => (
             <li
               key={link.id}
-              className="text-[20px] pl-10 font-medium cursor-pointer"
+              className="text-[20px] pl-10 font-medium cursor-pointer p-3"
               >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
@@ -60,4 +62,4 @@ function Footer() {
   )
 }
 
-export default SectionWrapper(Footer, 'footer')
+export default Footer;

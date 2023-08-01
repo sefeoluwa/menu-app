a65fa815e86b27eb7b89cec5e4102d5eb2dbd665
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-01 22:32:12 +0100

    added tilt to images

--- src/components/GetApp.jsx
+++ src/components/GetApp.jsx
@@ -3,23 +3,27 @@ import { motion } from "framer-motion";
 import SectionWrapper from '../hoc/SectionWrapper';
 import { download } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { Tilt } from "react-tilt";
 
 
 function GetApp() {
   return (
     <>
       <motion.p variants={textVariant()} className="flex justify-center text-[24px] mt-9 get-text">Get the App and begin to search the menu of your favourte restaurants.</motion.p>
-      <motion.div variants={fadeIn()} className="flex justify-center items-center  p-5">
+     
+     <motion.div variants={fadeIn()} className="flex justify-center items-center  p-5">
         {download.map((download) => { 
           return (
+            <Tilt key={download.name} >
           <img 
-          key={download.name} 
           src={download.image}
           className="p-6 cursor-pointer app-links"
            />
+           </Tilt>
         )
         })}
       </motion.div>
+    
     </>
   )
 }

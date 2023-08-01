61d9a39b5f98061881598ea78fb92b7ed5c385ba
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-01 18:32:39 +0100

    added classes for css

--- src/components/GetApp.jsx
+++ src/components/GetApp.jsx
@@ -8,14 +8,14 @@ import { fadeIn, textVariant } from "../utils/motion";
 function GetApp() {
   return (
     <>
-      <motion.p variants={textVariant()} className="flex justify-center text-[24px] mt-9">Get the App and begin to search the menu of your favourte restaurants.</motion.p>
+      <motion.p variants={textVariant()} className="flex justify-center text-[24px] mt-9 get-text">Get the App and begin to search the menu of your favourte restaurants.</motion.p>
       <motion.div variants={fadeIn()} className="flex justify-center items-center  p-5">
         {download.map((download) => { 
           return (
           <img 
           key={download.name} 
           src={download.image}
-          className="p-6 cursor-pointer"
+          className="p-6 cursor-pointer app-links"
            />
         )
         })}

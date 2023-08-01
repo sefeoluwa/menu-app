1e63daaf57c0fbc6f48d30f081ab60e78311ed98
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-01 17:26:52 +0100

    alignment for images on mobiles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,15 +10,15 @@ import { Tilt } from 'react-tilt';
 function About() {
   return (
     <>
-    <div className='h-[60vh] overflow-visible'>
+    <div className='h-[60vh] about overflow-visible'>
 
     <motion.div variants={textVariant()}>
-        <h2 className={styles.aboutHeadText} >No impromptu billings</h2>
-        <h2 className={styles.aboutHeadText} >No added charges</h2>
-        <h2 className={styles.aboutHeadText} >All within your keyboard reach</h2>
+        <h2 className={`${styles.aboutHeadText} about-heading`} >No impromptu billings</h2>
+        <h2 className={`${styles.aboutHeadText} about-heading`} >No added charges</h2>
+        <h2 className={`${styles.aboutHeadText} about-heading`} >All within your keyboard reach</h2>
       </motion.div>
 
-      <motion.div variants={fadeIn("right", "slide", "0.8")} className='flex justify-center gap-14 pt-[30vh]  items-center h-2/4'>
+      <motion.div variants={fadeIn("right", "slide", "0.8")} className='flex justify-center gap-14 pt-[30vh] about-screens  items-center h-2/4'>
         {screens.map((screen) => (
           <Tilt
             key={screen.name}
